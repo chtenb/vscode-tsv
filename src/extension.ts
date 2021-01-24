2c2ce6f117b2ffde162186e2b7c9f9fdd216d6d6
Chiel ten Brinke <ctenbrinke@gmail.com>
2021-01-24 11:08:42 +0100

    First working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let disposables = [
+		vscode.window.onDidChangeActiveTextEditor((editor) => {
+			adjustTabSize(editor);
+		}),
+		vscode.workspace.onDidSaveTextDocument(() => {
+			adjustTabSize(vscode.window.activeTextEditor);
+		})
+	];
+	context.subscriptions.push(...disposables);
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-tsv" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-tsv.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-tsv!');
-	});
-
-	context.subscriptions.push(disposable);
+function adjustTabSize(editor: vscode.TextEditor | undefined) {
+	if (!editor) {
+		return;
+	}
+	if (editor.document.languageId !== "tsv") {
+		return;
+	}
+	let text = editor.document.getText();
+	let cellSizes = text.split(/\r\n|\r|\n|\t/).map(cell => cell.length);
+	let maxCellSize = Math.max(...cellSizes);
+	editor.options.tabSize = maxCellSize + 1;
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
